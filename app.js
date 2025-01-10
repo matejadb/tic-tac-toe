f98cbf7d741a74fd615b947b390d57d75ae2319b
matejadb <92729211+matejadb@users.noreply.github.com>
2025-01-10 13:49:43 +0100

    clean code

--- app.js
+++ app.js
@@ -167,25 +167,19 @@ function ScreenController() {
 	const boardDiv = document.querySelector(".board");
 
 	const updateScreen = () => {
-		// clear the board
 		boardDiv.textContent = "";
 
-		// get the newest version of the board and player turn
 		const board = game.getBoard();
 		const activePlayer = game.getActivePlayer();
 
-		// Display player's turn
 		playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
 
-		// Render board squares
 		board.forEach((row, rowIndex) => {
 			row.forEach((cell, columnIndex) => {
-				// Anything clickable should be a button!!
 				const cellButton = document.createElement("button");
 				cellButton.classList.add("cell");
 				cellButton.classList.add("jersey-10-regular");
-				// Create a data attribute to identify the column
-				// This makes it easier to pass into our `playRound` function
+
 				cellButton.dataset.row = rowIndex;
 				cellButton.dataset.column = columnIndex;
 				cellButton.textContent = cell.getValue();
@@ -197,7 +191,7 @@ function ScreenController() {
 	function clickHandlerBoard(e) {
 		const selectedColumn = e.target.dataset.column;
 		const selectedRow = e.target.dataset.row;
-		// Make sure I've clicked a column and not the gaps in between
+
 		if (!selectedColumn || !selectedRow) return;
 
 		game.playRound(selectedRow, selectedColumn);
